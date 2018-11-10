03cd36b4152e56ab7df98c9805ef221b03833aa9
Th3hoopman <stayvaliant94@gmail.com>
2018-11-09 18:59:58 -0800

    - Added logo asset - Added logo to header - Added header to App

--- on-feet/src/components/Header.js
+++ on-feet/src/components/Header.js
@@ -1,12 +1,18 @@
+import React, { Component } from 'react';
+
 // Props:
 //
 // Component Function:
 // - Shows logo
 // - Search box filters preview image based on name and tags in real time
-
-class Header extends React.Component (
+import logo from '../assets/of-logo-01.svg';
+class Header extends Component {
   render(){
-    //logo
-    //search bar
+    return(
+      <img src={logo}/>
+      //search bar
+    );
   }
-)
+}
+
+export default Header;
