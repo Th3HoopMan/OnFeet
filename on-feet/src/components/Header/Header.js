76f928c2cddef9f6c0fa9d3e9f6cac8f74ac1980
Joseph Hooper <stayvaliant94@gmail.com>
2018-11-09 19:41:07 -0800

    Added bootstrap
    - Styled header a little

--- on-feet/src/components/Header/Header.js
+++ on-feet/src/components/Header/Header.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-
+import './Header.css';
 // Props:
 //
 // Component Function:
 // - Shows logo
 // - Search box filters preview image based on name and tags in real time
-import logo from '../assets/of-logo-01.svg';
+import logo from '../../assets/of-logo-01.svg';
 class Header extends Component {
   render(){
     return(
-      <img src={logo}/>
-      //search bar
+      <div>
+        <img src={logo} class="logo"/>
+        <input/>
+      </div>
+
     );
   }
 }
