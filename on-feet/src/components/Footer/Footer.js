a4988d390f31cfa188da5af4d0576f8afafb4854
Th3hoopman <stayvaliant94@gmail.com>
2018-11-11 19:31:26 -0800

    Additions to the footer and header.

---
+++ on-feet/src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './Footer.css'
+// Props:
+//S
+// Component Function:
+// - About Overlay
+// - Support Overlay
+
+class Footer extends Component {
+  render(){
+    return(
+      <div class="main">
+        <p>about.</p>
+        <p>support.</p>
+      </div>
+    );
+  }
+}
+
+export default Footer
